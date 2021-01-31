c65808db7d313c4f9b38873ff7f749c9c6666b5e
Rodrigo Aleman B <rabcr.14@gmail.com>
2021-01-31 01:02:42 -0600

    feat: Add all form tests

--- norascafeteria-project/cafeteria/tests.py
+++ norascafeteria-project/cafeteria/tests.py
@@ -1,9 +1,11 @@
+from http import HTTPStatus
 from django.test import TestCase
 from uuid import UUID
 from django.utils.timezone import now, localtime
 from django.db import IntegrityError
 
 from .models import Dish, User, Menu, Order
+from .forms import DishForm, MenuForm, OrderForm
 
 
 # All Model tests
@@ -78,3 +80,69 @@ class OrderTest(TestCase):
         with self.assertRaises(IntegrityError) as raise_context:
             self.create_order()
             self.assertTrue('UNIQUE constraint failed' in raise_context.exception.message)
+
+
+# All Form tests
+class DishFormTest(TestCase):
+
+    def test_valid_dish_form(self):
+        data = {'name': 'Other dish'}
+        form = DishForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_error_dish_form(self):
+        dish = Dish.objects.create(name='Premium chicken Salad and Dessert')
+        data = {'name': dish.name}
+        form = DishForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["name"], ["Dish with this Name already exists."])
+
+
+class MenuFormTest(TestCase):
+
+    def test_valid_menu_form(self):
+        dish = Dish.objects.create(name='foo')
+        data = {'date': localtime(now()).date(), 'detail': 'None', 'dishes': [dish]}
+        form = MenuForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_error_menu_form(self):
+        data = {'date': localtime(now()).date(), 'detail': 'None', 'dishes': None}
+        form = MenuForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["dishes"], ["This field is required."])
+        data = {'date': localtime(now()).date(), 'detail': '', 'dishes': [{"name": "foo"}]}
+        form = MenuForm(data=data)
+        self.assertEqual(form.errors["dishes"], ["Enter a list of values."])
+        self.assertEqual(form.errors["detail"], ["This field is required."])
+
+        menu = Menu()
+        menu.detail = "Today's menu"
+        menu.dishes.set(Dish.objects.all())
+        menu.date = localtime(now()).date()
+        menu.save()
+
+        dish = Dish.objects.create(name='foo')
+        data = {'date': localtime(now()).date(), 'detail': 'None', 'dishes': [dish]}
+        form = MenuForm(data=data)
+        self.assertEqual(form.errors["date"], ["Menu with this Date already exists."])
+
+
+class OrderFormTest(TestCase):
+
+    def test_valid_order_form(self):
+        dish = Dish.objects.create(name='foo')
+        data = {'dish': dish, 'customizations': ''}
+        form = OrderForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_error_order_form(self):
+        data = {'dish': 'foo', 'customizations': ''}
+        form = OrderForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["dish"], ["Select a valid choice. That choice is not one of the available choices."])
+        data = {'dish': None, 'employee': 'test', 'customizations': ''}
+        form = OrderForm(data=data)
+        self.assertEqual(form.errors["dish"], ["This field is required."])
+
+
