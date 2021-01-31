918588f21aa4ed16e7e43ba61445dc14a8662362
Rodrigo Aleman B <rabcr.14@gmail.com>
2021-01-30 21:10:51 -0600

    feat: Add page to edit menus

--- norascafeteria-project/settings/urls.py
+++ norascafeteria-project/settings/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     path('dish_form', views.dish_form, name='dish_form'),
     path('dish_form/<int:pk>', views.edit_dish, name='edit_dish'),
     path('menu_form', views.menu_form, name='menu_form'),
-
+    path('menu_form/<str:pk>', views.edit_menu, name='edit_menu'),
     path('see_orders', views.see_orders, name='see_orders'),
-
     path('menu', views.redirect_uuid, name='menu'),
     path('menu/<str:pk>', views.order_uuid, name='menu'),
 ]
